c5e8544b6fedb3c57fb97e077132a1128e377a64
Saparmurat <saparmurat@protonmail.com>
2022-10-01 20:46:01 +0600

    setup models: Book, Genre, Author, BookInstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Book, BookInstance, Author, Genre
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
+admin.site.register(Genre)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,90 @@
 from django.db import models
-
+from django.urls import reverse
+import uuid
 # Create your models here.
+
+
+class Genre(models.Model):
+    # Model for book genres
+    name = models.CharField(max_length=200, help_text='Enter a book genre.')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(
+        max_length=1000, help_text='Enter a brief description of the book')
+
+    # isbn
+    link = '<a href = "https://www.isbn-international.org/content/what-isbn" >'
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                            help_text=f'13 Character {link} ISBN number</a>')
+
+    # many to many relationship
+    genre = models.ManyToManyField(
+        Genre, help_text='Select genre for this book')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book')
+
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+
+    imprint = models.CharField(max_length=200)
+
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book Availability'
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.id})'
+
+
+class Author(models.Model):
+    """Model representing an author."""
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
