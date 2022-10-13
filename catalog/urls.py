44d749fe67ec20c2c74c7e24d943b397f9fc5ca6
Saparmurat <saparmurat@protonmail.com>
2022-10-13 16:41:06 +0600

    set up routing for book

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-
+    path('books/', views.books_list),
+    path('books/<int:pk>/', views.book_details)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
